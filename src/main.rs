46ce5e77f8085bc9a7c1a9af9a40b96c7e88604a
Caer <create@caer.cc>
2025-08-21 07:49:40 -0400

    feat: add tab selection for the neutral and accent color groups

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use ratatui::{
     layout::{Constraint, Flex, Layout, Rect},
     style::{Color, Stylize},
     text::Text,
-    widgets::{Block, Paragraph, Widget},
+    widgets::{Block, BorderType, Padding, Paragraph, Widget},
 };
 
 /// The default neutral color loaded on application start.
@@ -45,8 +45,21 @@ struct App {
 /// This widget is animated and will change colors over time.
 #[derive(Debug, Default)]
 struct ColorsWidget {
-    neutral_color: cate::Color,
+    /// The base neutral color from which all neutral tones are derived.
+    base_neutral_color: cate::Color,
+
+    /// The chromaticity of the base accent color.
+    base_accent_chromaticity: f32,
+
+    /// The hue offset of the base accent color relative
+    /// to the base neutral color.
+    base_accent_hue_offset: f32,
+
+    /// Iff true, colors will be fitted into a CMYK gamut.
     cmyk_gamut_fitting: bool,
+
+    /// The currently selected color block in the UI.
+    active_color_block_index: usize,
 }
 
 impl App {
@@ -54,8 +67,15 @@ impl App {
     ///
     /// This is the main event loop for the app.
     pub fn run(mut self, mut terminal: DefaultTerminal) -> std::io::Result<()> {
-        self.colors_widget.neutral_color =
+        self.colors_widget.base_neutral_color =
             cate::Color::try_from_hex(DEFAULT_NEUTRAL_COLOR.into()).unwrap();
+        self.colors_widget.base_accent_chromaticity = self
+            .colors_widget
+            .base_neutral_color
+            .c
+            .max(ACCENT_MIN_CHROMA);
+        self.colors_widget.base_accent_hue_offset = ACCENT_HUE_STEP;
+        self.colors_widget.active_color_block_index = 0;
 
         loop {
             terminal.draw(|frame| frame.render_widget(&mut self, frame.area()))?;
@@ -91,15 +111,16 @@ impl App {
 
                 // Copy the current neutral colors to the keyboard as SCSS RGBA colors.
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('w') {
-                    let mut neutrals =
-                        cate::Neutrals::from_color_hue_adjusted(&self.colors_widget.neutral_color);
+                    let mut neutrals = cate::Neutrals::from_color_hue_adjusted(
+                        &self.colors_widget.base_neutral_color,
+                    );
 
                     let base_color_str = format!(
                         "{} (sRGB HEX) | oklch({:.2} {:.3} {:.2})",
-                        &self.colors_widget.neutral_color,
-                        self.colors_widget.neutral_color.l,
-                        self.colors_widget.neutral_color.c,
-                        self.colors_widget.neutral_color.h,
+                        &self.colors_widget.base_neutral_color,
+                        self.colors_widget.base_neutral_color.l,
+                        self.colors_widget.base_neutral_color.c,
+                        self.colors_widget.base_neutral_color.h,
                     );
 
                     let gamut_str = if self.colors_widget.cmyk_gamut_fitting {
@@ -139,20 +160,51 @@ $c-darkest:  rgba({}, 1); // L={:.2} {gamut_str}"#,
                     return Ok(true);
                 }
 
+                // Cycle selected colors on tab.
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Tab {
+                    self.colors_widget.active_color_block_index += 1;
+                    self.colors_widget.active_color_block_index =
+                        self.colors_widget.active_color_block_index % 2;
+                    return Ok(true);
+                }
+
                 // Handle input events for the neutral color.
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Right {
-                    self.colors_widget.neutral_color.h =
-                        (self.colors_widget.neutral_color.h + 1.0) % 360.0;
+                    if self.colors_widget.active_color_block_index == 0 {
+                        self.colors_widget.base_neutral_color.h =
+                            (self.colors_widget.base_neutral_color.h + 1.0) % 360.0;
+                    } else {
+                        self.colors_widget.base_accent_hue_offset =
+                            (self.colors_widget.base_accent_hue_offset + 1.0) % 360.0;
+                    }
                 } else if key.kind == KeyEventKind::Press && key.code == KeyCode::Left {
-                    self.colors_widget.neutral_color.h =
-                        (self.colors_widget.neutral_color.h - 1.0) % 360.0;
+                    if self.colors_widget.active_color_block_index == 0 {
+                        self.colors_widget.base_neutral_color.h =
+                            (self.colors_widget.base_neutral_color.h - 1.0) % 360.0;
+                    } else {
+                        self.colors_widget.base_accent_hue_offset =
+                            (self.colors_widget.base_accent_hue_offset - 1.0) % 360.0;
+                    }
                 } else if key.kind == KeyEventKind::Press && key.code == KeyCode::Up {
-                    self.colors_widget.neutral_color.c = (self.colors_widget.neutral_color.c
-                        + NEUTRAL_CHROMA_STEP)
-                        .min(NEUTRAL_MAX_CHROMA);
+                    if self.colors_widget.active_color_block_index == 0 {
+                        self.colors_widget.base_neutral_color.c =
+                            (self.colors_widget.base_neutral_color.c + NEUTRAL_CHROMA_STEP)
+                                .min(NEUTRAL_MAX_CHROMA);
+                    } else {
+                        self.colors_widget.base_accent_chromaticity =
+                            (self.colors_widget.base_accent_chromaticity + NEUTRAL_CHROMA_STEP)
+                                .min(NEUTRAL_MAX_CHROMA);
+                    }
                 } else if key.kind == KeyEventKind::Press && key.code == KeyCode::Down {
-                    self.colors_widget.neutral_color.c =
-                        (self.colors_widget.neutral_color.c - NEUTRAL_CHROMA_STEP).max(0.0);
+                    if self.colors_widget.active_color_block_index == 0 {
+                        self.colors_widget.base_neutral_color.c =
+                            (self.colors_widget.base_neutral_color.c - NEUTRAL_CHROMA_STEP)
+                                .max(0.0);
+                    } else {
+                        self.colors_widget.base_accent_chromaticity =
+                            (self.colors_widget.base_accent_chromaticity - NEUTRAL_CHROMA_STEP)
+                                .max(0.0);
+                    }
                 }
             }
         }
@@ -172,8 +224,8 @@ impl Widget for &mut App {
         let [_] = Layout::horizontal([Min(0)]).areas(top);
         let [instructions_area] = Layout::horizontal([Min(0)]).areas(bottom);
 
-        let base_chroma = format!("{:0.3}", self.colors_widget.neutral_color.c);
-        let base_hue: String = format!("{:0.2}", self.colors_widget.neutral_color.h);
+        let base_chroma = format!("{:0.3}", self.colors_widget.base_neutral_color.c);
+        let base_hue: String = format!("{:0.2}", self.colors_widget.base_neutral_color.h);
 
         let g_label = if self.colors_widget.cmyk_gamut_fitting {
             "Disable"
@@ -197,6 +249,12 @@ impl Widget for &mut App {
 /// cached version of the colors to render instead of recalculating them every frame.
 impl Widget for &mut ColorsWidget {
     fn render(self, area: Rect, buf: &mut Buffer) {
+        // Generate the neutral colors.
+        let mut neutrals = cate::Neutrals::from_color_hue_adjusted(&self.base_neutral_color);
+        if self.cmyk_gamut_fitting {
+            neutrals = neutrals.to_cmyk_adjusted();
+        }
+
         // Render a column for each neutral color.
         let neutral_colors = 7;
         let col_constraints = (0..neutral_colors).map(|_| Constraint::Min(9));
@@ -204,20 +262,39 @@ impl Widget for &mut ColorsWidget {
         // Render two rows of colors (one for neutrals, one for accents).
         let row_constraints = (0..2).map(|_| Constraint::Min(3));
 
-        // Split the rendered area into cells.
+        // Split the rendered area into vertical rows.
         let horizontal = Layout::horizontal(col_constraints).spacing(1);
         let vertical = Layout::vertical(row_constraints).spacing(1);
         let rows = vertical.split(area);
-        let cells = rows
-            .iter()
-            .flat_map(|&row| horizontal.split(row).to_vec())
-            .collect::<Vec<_>>();
 
-        // Generate the neutral colors.
-        let mut neutrals = cate::Neutrals::from_color_hue_adjusted(&self.neutral_color);
-        if self.cmyk_gamut_fitting {
-            neutrals = neutrals.to_cmyk_adjusted();
+        // Split rows into cells.
+        let mut cells = vec![];
+        for (i, row) in rows.iter().enumerate() {
+            // Wrap each row in a block.
+            let mut block = Block::new();
+
+            // Wrap a border around the active block.
+            if i == self.active_color_block_index {
+                let [r, g, b] = neutrals.neutral.to_srgb();
+                let border_color =
+                    Color::Rgb((r * 255.0) as u8, (g * 255.0) as u8, (b * 255.0) as u8);
+                block = Block::bordered()
+                    .border_type(BorderType::Thick)
+                    .border_style(border_color);
+
+            // Pad inactive blocks so that bordered and unbordered
+            // blocks appear to have the same inner area.
+            } else {
+                block = block.padding(Padding::uniform(1));
+            }
+            let block_area = block.inner(*row);
+            block.render(*row, buf);
+
+            // Split the row into cells.
+            cells.extend_from_slice(&*horizontal.split(block_area));
         }
+
+        // Convert neutrals into a list for rendering.
         let neutral = neutrals.neutral.clone();
         let neutrals = neutrals.into_iter().collect::<Vec<_>>();
 
@@ -230,8 +307,9 @@ impl Widget for &mut ColorsWidget {
         for (i, cell) in cells.iter().skip(neutral_colors).enumerate() {
             // Derive the accent color.
             let mut color = neutral.clone();
-            color.h = (neutral.h + (ACCENT_HUE_STEP * (1.0 + i as f32))) % 360.0;
-            color.c = color.c.max(ACCENT_MIN_CHROMA);
+            color.h =
+                ((neutral.h + self.base_accent_hue_offset) + (ACCENT_HUE_STEP * i as f32)) % 360.0;
+            color.c = self.base_accent_chromaticity;
 
             // Derive the tones of the accent color.
             let mut tones = cate::Neutrals::from_color_hue_adjusted(&color);
